f1d8a04cc0d971fca347db735adab4a0c2197805
Justin Ferre <Jman4647@gmail.com>
2015-10-15 18:36:45 -0400

    fixed issue related to Cocoapod download

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,48 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Justin Ferre on 10/15/15.
+//  Copyright © 2015 Justin Ferre. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    @IBOutlet weak var namelbl: UILabel!
+    @IBOutlet weak var mainImgLbl: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var PokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        namelbl.text = pokemon.name
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+
+}
