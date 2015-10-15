2879086697780b90431b93081a72a7b67fac66af
Justin Ferre <Jman4647@gmail.com>
2015-10-15 11:43:07 -0400

    added detail view controller and segue

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,38 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Justin Ferre on 10/15/15.
+//  Copyright © 2015 Justin Ferre. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    @IBOutlet weak var namelbl: UILabel!
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        namelbl.text = pokemon.name
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
